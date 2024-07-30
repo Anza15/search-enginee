04baac95670ce2f45c496e7b1f0c4a1544c3ff19
Anza15 <mukhiithianza1@gmail.com>
2024-07-30 23:01:35 +0200

    added bootstrap

---
+++ search-engine/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+       <div className="container">
+      
+     <h1>Weather App</h1>
+          <footer>
+            This project was coded by {""}
+            <a href="#" target="_blank"
+            rel="noopener noreferrer">
+            Anza Mukhithi
+          </a>{""}
+          and is {""}
+          <a href="#" target="_blank"
+          rel="noopener noreferrer">
+            open-source on Github
+            and {""}
+            <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+  
+        </a>
+      </footer>
+      </div>
+      </div>
+  );
+}
+
