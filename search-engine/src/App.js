88e328fd6fa64e25b7a9bdf27e40d7e3ef030976
Anza15 <mukhiithianza1@gmail.com>
2024-07-31 16:28:27 +0200

    updated defaultcity

--- search-engine/src/App.js
+++ search-engine/src/App.js
@@ -10,7 +10,7 @@ export default function App() {
        <div className="container">
       
        <h1>Weather App</h1>
-       <Weather />
+       <Weather defaultCity="Pretoria"/>
           <footer>
             This project was coded by {""}
             <a href="#" target="_blank"
