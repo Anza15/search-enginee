578671e863c124a67bef02826d7bc66bd3d08e07
Anza15 <mukhiithianza1@gmail.com>
2024-08-02 07:16:22 +0200

    Updated my Weather.js and icon

--- search-engine/src/Weather.js
+++ search-engine/src/Weather.js
@@ -1,69 +1,70 @@
 import React, { useState } from "react";
-import axios from "axios";
-import "./Weather.css";
-
-
-export default function Weather(props) {
-  const [weatherData, setWeatherData] = useState({ ready: false });
-  const [city, setCity] = useState(props.defaultCity);
-
-  function handleResponse(response) {
-    setWeatherData({
-      ready: true,
-      coordinates: response.data.coord,
-      temperature: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      date: new Date(response.data.dt * 1000),
-      description: response.data.weather[0].description,
-      icon: response.data.weather[0].icon,
-      wind: response.data.wind.speed,
-      city: response.data.name,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-
-  function handleCityChange(event) {
-    setCity(event.target.value);
-  }
-
-  function search() {
-    const apiKey =`o3bfb209f55e8951210f40e6476fat3f`;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-
-  if (weatherData.ready) {
-    return (
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city.."
-                className="form-control"
-                autoFocus="on"
-                onChange={handleCityChange}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
-       
-      </div>
-    );
-  } else {
-    search();
-    return "Loading...";
-  }
-}
+ import axios from "axios";
+ import "./Weather.css";
+ import WeatherInfo from "./WeatherInfo"
+ 
+ 
+ export default function Weather(props) {
+   const [weatherData, setWeatherData] = useState({ ready: false });
+   const [city, setCity] = useState(props.defaultCity);
+ 
+   function handleResponse(response) {
+     setWeatherData({
+       ready: true,
+       coordinates: response.data.coordinates,
+       temperature: response.data.temperature.current,
+       humidity: response.data.temperature.humidity,
+       date: new Date(response.data.time * 1000),
+       description: response.data.condition.description,
+       icon: response.data.condition.icon,
+       wind: response.data.wind.speed,
+       city: response.data.city,
+     });
+   }
+ 
+   function handleSubmit(event) {
+     event.preventDefault();
+     search();
+   }
+ 
+   function handleCityChange(event) {
+     setCity(event.target.value);
+   }
+ 
+   function search() {
+     const apiKey =`o3bfb209f55e8951210f40e6476fat3f`;
+     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+     axios.get(apiUrl).then(handleResponse);
+   }
+ 
+   if (weatherData.ready) {
+     return (
+       <div className="Weather">
+         <form onSubmit={handleSubmit}>
+           <div className="row">
+             <div className="col-9">
+               <input
+                 type="search"
+                 placeholder="Enter a city.."
+                 className="form-control"
+                 autoFocus="on"
+                 onChange={handleCityChange}
+               />
+             </div>
+             <div className="col-3">
+               <input
+                 type="submit"
+                 value="Search"
+                 className="btn btn-primary w-100"
+               />
+             </div>
+           </div>
+         </form>
+         <WeatherInfo data={weatherData} />
+       </div>
+     );
+   } else {
+     search();
+     return "Loading...";
+   }
+ }
